6f04d52a697b29ab216189c5ada72dc334793b56
Paula Garcia <pauligarcia_05@hotmail.com>
2024-07-20 12:44:09 -0300

    Section 3: Readme added and syntax errors fix

--- react-course-projects/Section_3/src/App.jsx
+++ react-course-projects/Section_3/src/App.jsx
@@ -47,8 +47,8 @@ function App() {
           <menu>
             <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
             <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           {tabContent}
         </section>
