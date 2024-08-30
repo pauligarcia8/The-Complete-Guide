7b7a2a10dd58dfe5f6834ac49b51d5a636f3784f
Paula <pauligarcia_05@hotmail.com>
2024-08-30 19:08:28 -0300

    Section 18: finish app

--- react-course-projects/Section_18/src/components/Cart.jsx
+++ react-course-projects/Section_18/src/components/Cart.jsx
@@ -5,6 +5,7 @@ import CartContext from '../store/CartContext.jsx';
 import Button from './UI/Button.jsx';
 import { currencyFormatter } from '../util/formatting.js';
 import UserProgressContext from '../store/UserProgressContext.jsx';
+import CartItem from './CartItem.jsx';
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -19,14 +20,27 @@ export default function Cart() {
     userProgressCtx.hideCart();
   }
 
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout();
+  }
+
   return (
-    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+    <Modal
+      className="cart"
+      open={userProgressCtx.progress === 'cart'}
+      onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}
+    >
       <h2>Your Cart</h2>
       <ul>
-      {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
@@ -34,7 +48,9 @@ export default function Cart() {
         <Button textOnly onClick={handleCloseCart}>
           Close
         </Button>
-        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+        )}
       </p>
     </Modal>
   );
--- react-course-projects/Section_18/src/store/UserProgressContext.jsx
+++ react-course-projects/Section_18/src/store/UserProgressContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react';
 
-export const UserProgressContext = createContext({
+const UserProgressContext = createContext({
   progress: '', // 'cart', 'checkout'
   showCart: () => {},
   hideCart: () => {},
