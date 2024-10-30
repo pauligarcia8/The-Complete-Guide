c7a129af76b04911850396f7cc0c54423b3a5924
Paula Garcia <86069162+pauligarcia8@users.noreply.github.com>
2024-10-30 17:13:48 -0300

    Section 21: add readme (#28)
    * Section 21: add readme
    
    * Section 21: finish first part

---
+++ react-course-projects/Section_21/src/App.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />}/>
+//     <Route path='/products' element={<Products />}/>
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "products", element: <Products /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
